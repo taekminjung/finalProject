af6ec3a7a93e4080eb556313fbddac2c62cbda08
GD <GD@DESKTOP-FIURBO1>
2023-12-21 16:28:52 +0900

    [택민] application.properties파일 한글깨짐 수정 및 AWS DB 연결 코드 작성

---
+++ haribo/src/main/java/com/example/haribo/service/HomeService.java
@@ -0,0 +1,10 @@
+package com.example.haribo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.haribo.vo.Customer;
+
+@Service
+public class HomeService {
+				
+}
---
+++ haribo/src/main/java/com/example/haribo/vo/Customer.java
@@ -0,0 +1,13 @@
+package com.example.haribo.vo;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+		private int customerNo;
+		private String customerId;
+		private String customerPw;
+		private String customerActive;
+		private String createdate;
+		private String updatedate;
+}
