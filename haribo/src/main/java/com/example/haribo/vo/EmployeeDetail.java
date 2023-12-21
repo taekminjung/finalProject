5fe0694be2e274c2f2e50b2363f816a27da998d0
GD <GD@DESKTOP-FIURBO1>
2023-12-21 17:28:26 +0900

    [택민] VO클래스 추가

---
+++ haribo/src/main/java/com/example/haribo/vo/EmployeeDetail.java
@@ -0,0 +1,15 @@
+package com.example.haribo.vo;
+
+import lombok.Data;
+
+@Data
+public class EmployeeDetail {
+		private int employeeNo;
+		private String employeeName;
+		private String employeePhone;
+		private String employeeEmail;
+		private String employeeGender;
+		private String createdate;
+		private String updatedate;
+		
+}
---
+++ haribo/src/main/java/com/example/haribo/vo/EmployeeImg.java
@@ -0,0 +1,15 @@
+package com.example.haribo.vo;
+
+import lombok.Data;
+
+@Data
+public class EmployeeImg {
+		private int employeeImgNo;
+		private int employeeNo;
+		private String employeeImgOriginName;
+		private String employeeImgFileName;
+		private String employeeImgSize;
+		private String employeeImgType;
+		private String createdate;
+		private String updatedate;
+}
