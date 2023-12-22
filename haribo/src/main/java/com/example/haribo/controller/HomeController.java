dc8a47ecbb682db43fb8578776a53e71ceb03fbc
hoe-yun <gmn04160@gmail.com>
2023-12-22 11:47:00 +0900

    [정회윤] home.jsp 추가 HomeController.java  "/home" 매핑추가 home.jsp 생성(탬플릿 홈 화면 그대로 - 추후 내용에 맞게 수정)

--- haribo/src/main/java/com/example/haribo/controller/HomeController.java
+++ haribo/src/main/java/com/example/haribo/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.example.haribo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
+	@GetMapping("/home")
+	public String home() {
+		
+		return "home";
+	}
 }
