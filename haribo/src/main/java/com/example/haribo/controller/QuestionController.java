b5dc070f1a01175ca7cdbf6bc988463bf90b6d2d
g-xxr <yoozzac@gmail.com>
2024-01-02 18:03:20 +0900

    [유정] 240102 오후 question 관련 필요 view 폴더 jsp 파일 추가 완료 question controller 작성 완료 questionService 작성중 ...

--- haribo/src/main/java/com/example/haribo/controller/QuestionController.java
+++ haribo/src/main/java/com/example/haribo/controller/QuestionController.java
@@ -10,13 +10,58 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.haribo.service.QuestionService;
-import com.example.haribo.vo.Notice;
 import com.example.haribo.vo.Question;
 
 @Controller
 public class QuestionController {
 	@Autowired private QuestionService questionService;
-	/*
+	
+	// 문의글 등록하기
+	@GetMapping("/insertQuestion")
+	public String insertQuestion() {
+		
+		return "public/insertQuestion";
+	}
+	
+	@PostMapping("/insertQuestion")
+	public String insertQuestion(Question question) {
+		//mapper 호출하기
+		QuestionService.insertQuestion(question);
+		
+		return "redirect:/public/question";
+	}
+	
+	// 문의 글 수정하기
+	@GetMapping("/updateQuestion")
+	public String updateQuestion(Question question, Model model) {
+		Question resultQuestion = questionService.questionOne(question);
+		model.addAttribute("resultQuestion" , resultQuestion);
+		
+		return "public/updateQuestion";
+	}
+
+	@PostMapping("/updateQuestion")
+	public String updateQuestion(Question question) {
+		int row = questionService.updateQuestion(question);
+		if(row == 0) {
+			System.out.println("업데이트 실패");
+			return "redirect:/updateQuestion?questionNo="+question.getQuestionNo();
+		} else {
+			System.out.println("업데이트 성공");
+		}
+			return "redirect:/public/questionList";
+	}
+	
+	// 문의 글 삭제하기
+	@GetMapping("/deleteQuestion")
+	public String deleteQuestion(Question question) {
+		questionService.deleteQuestion(question);
+		
+		return "redirect:/public/questionList";
+	}
+	
+
+	// 문의사항 리스트(고객)
 	@GetMapping("/questionList")
 	public String questionList(Model model, @RequestParam(defaultValue ="1")int currentPage) {
 		List<Question> list = questionService.questionList(currentPage);
@@ -29,21 +74,50 @@ public class QuestionController {
 		
 		return "emp/questionList";
 	}
+	// 문의사항 상세보기(고객)
+	@GetMapping("/questionOne")
+	public String questionOne(Model model, Question question) {
+		Question resultQuestion = questionService.questionOne(question);
+		model.addAttribute("resultQuestion", resultQuestion);
+		System.out.println(resultQuestion.getQuestionNo()+"<----getNoticeNo");
+		System.out.println(resultQuestion.getQuestionTitle()+"<----getNoticeTitle");
+		System.out.println(resultQuestion.getQuestionContent()+"<----getNoticeContent");
+		System.out.println(resultQuestion.getCustomerId()+"<----getCustomerId");
+		System.out.println(resultQuestion.getCreatedate()+"<----getCreatedate");
 	
-	@GetMapping("/insertQuestion")
-	public String insertQuestion() {
-		
-		return "public/question";
+		return "emp/trainerQuestionOne";
 	}
 	
-	@PostMapping("/insertQuestion")
-	public String insertQuestion(Question question) {
-		//mapper 호출하기
-		QuestionService.insertQuestion(question);
+	// 문의사항 리스트(직원)
+	@GetMapping("/question")
+	public String question(Model model, @RequestParam(defaultValue ="1")int currentPage) {
+		List<Question> list = questionService.questionList(currentPage);
+		int lastPage = questionService.lastPage();
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("list", list);
+		model.addAttribute("lastPage", lastPage);
 		
-		return "redirect:/public/question";
+		System.out.println(lastPage+"<--lastPage");
+		
+		return "emp/question";
 	}
 	
+	
+	// 문의사항 상세보기(직원)
+	@GetMapping("/questionOneEmp")
+	public String questionOneEmp(Model model, Question question) {
+		Question resultQuestion = questionService.questionOne(question);
+		model.addAttribute("resultQuestion", resultQuestion);
+		System.out.println(resultQuestion.getQuestionNo()+"<----getNoticeNo");
+		System.out.println(resultQuestion.getQuestionTitle()+"<----getNoticeTitle");
+		System.out.println(resultQuestion.getQuestionContent()+"<----getNoticeContent");
+		System.out.println(resultQuestion.getCustomerId()+"<----getCustomerId");
+		System.out.println(resultQuestion.getCreatedate()+"<----getCreatedate");
+	
+		return "emp/questionOne";
+	}
+	
+	// 문의글 리스트(트레이너)
 	@GetMapping("/trainerQuestion")
 	public String trainerQuestion(Model model, @RequestParam(defaultValue ="1")int currentPage) {
 		List<Question> list = questionService.questionList(currentPage);
@@ -56,6 +130,8 @@ public class QuestionController {
 				
 		return "emp/trainerQuestion";
 	}
+	
+	// 문의 글 상세보기(트레이너)
 	@GetMapping("/trainerQuestionOne")
 	public String trainerQuestionOne(Model model, Question question) {
 		Question resultQuestion = questionService.questionOne(question);
@@ -68,5 +144,4 @@ public class QuestionController {
 	
 		return "emp/trainerQuestionOne";
 	}
-	*/
 }
--- haribo/src/main/java/com/example/haribo/service/QuestionService.java
+++ haribo/src/main/java/com/example/haribo/service/QuestionService.java
@@ -1,10 +1,13 @@
 package com.example.haribo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.haribo.mapper.QuestionMapper;
+import com.example.haribo.vo.Question;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,6 +16,38 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class QuestionService {
 	@Autowired private QuestionMapper questionMapper;
+
+	public List<Question> questionList(int currentPage) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int lastPage() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static void insertQuestion(Question question) {
+		// TODO Auto-generated method stub
+		return;
+	}
+
+	public Question questionOne(Question question) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int updateQuestion(Question question) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void deleteQuestion(Question question) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 	
 
 
