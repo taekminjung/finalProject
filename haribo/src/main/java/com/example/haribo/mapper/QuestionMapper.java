3d41f7d8ddbdfeacef40fc83169fba9b98be9297
g-xxr <yoozzac@gmail.com>
2024-01-03 10:43:42 +0900

    [유정] question 관련 controller, mapper 작성완료

--- haribo/src/main/java/com/example/haribo/mapper/QuestionMapper.java
+++ haribo/src/main/java/com/example/haribo/mapper/QuestionMapper.java
@@ -1,8 +1,26 @@
 package com.example.haribo.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.example.haribo.vo.Question;
 
 @Mapper
 public interface QuestionMapper {
+		
+	int insertQuestion(Question question);
+	
+	int updateQuestion(Question question);
+	
+	int deleteQuestion(Question question);
+	
+	List<Question> questionList (Map<String, Object> param);
 
+	Question questionOne(Question question);
+		
+	int getTotalRow(int rowPerPage);
+	
+	List<Question> searchByQuestion(Question question);
 }
