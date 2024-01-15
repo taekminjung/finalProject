51ec4525a1027f8da555c5de8c54fe18f3ab045f
g-xxr <yoozzac@gmail.com>
2024-01-15 15:47:48 +0900

    주석처리 했던 것 되돌림

--- haribo/src/main/java/com/example/haribo/controller/TrainerHomeController.java
+++ haribo/src/main/java/com/example/haribo/controller/TrainerHomeController.java
@@ -16,6 +16,7 @@ import com.example.haribo.service.CalendarService;
 import com.example.haribo.service.EmployeeService;
 import com.example.haribo.service.ProgramService;
 import com.example.haribo.service.SportsEquipmentService;
+import com.example.haribo.vo.Branch;
 import com.example.haribo.vo.Employee;
 import com.example.haribo.vo.EmployeeImg;
 import com.example.haribo.vo.Program;
@@ -107,19 +108,20 @@ public class TrainerHomeController {
 	
 	// 재고관리(트레이너)
 	
-//	// 트레이너 지점 별 재고 현황 확인하기
-//	@GetMapping("/trainerStock")
-//	public String trainerStockList(Model model, @RequestParam(defaultValue="1") int currentPage, SportsEquipment sportsEquipment) {
-//		List<HashMap<String, Object>> stockList = sportsEquipmentService.trainerStockList(currentPage, sportsEquipment);
-//		
-//		int lastPageSp = sportsEquipmentService.lastPageSp();
-//	
-//		model.addAttribute("stockList", stockList);
-//		model.addAttribute("lastPageSp", lastPageSp);
-//		model.addAttribute("currentPage", currentPage);
-//		
-//		return "emp/trainerStock";
-//	}
+	// 트레이너 지점 별 재고 현황 확인하기
+	@GetMapping("/trainerStock")
+	public String trainerStockList(Model model, @RequestParam(defaultValue="1") int currentPage, Branch branch) {
+		List<HashMap<String, Object>> stockList = sportsEquipmentService.stockList(currentPage, branch);
+		
+		int lastPageStock = sportsEquipmentService.lastPageStock();
+		
+		model.addAttribute("stockList", stockList);
+		model.addAttribute("branch", branch);
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("lastPageStock", lastPageStock);
+		
+		return "emp/trainerStock";
+	}
 		
 	// 트레이너 물품 발주 입력하기(form+insert) + sportsEquipmentOrderForm에 본사가 발주한 물품 리스트 출력
 	@GetMapping("/sportsEquipmentOrderForm")
--- haribo/src/main/webapp/WEB-INF/view/emp/trainerNotice.jsp
+++ haribo/src/main/webapp/WEB-INF/view/emp/trainerNotice.jsp
@@ -35,17 +35,18 @@
     <section class="content-header">
     <h3>공지사항</h3>
     <div class="box-tools">
-                <div class="input-group input-group-sm hidden-xs" style="width: 150px;">
-                  <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
-
-                  <div class="input-group-btn">
-                    <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-                  </div>
-                </div>
-              </div>
+      <div class="input-group input-group-sm hidden-xs" style="width: 150px;">
+        <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
+    
+        <div class="input-group-btn">
+          <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+        </div>
+      </div>
+    </div>
+    <br>
       <div class="row">
         <div class="col-xs-12">
-          <div class="box">
+          <div class="box box-info">
             <div class="box-body table-responsive no-padding text-center">
               <table class="table table-hover text-center">
                 <tr>
